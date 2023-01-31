8b4320215fe424b40ec2a587caa917b256fac7d3
suleyman <gameclub2015@mail.ru>
2023-01-31 15:28:01 +0500

    'ownPage'

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,4 +6,5 @@ const router = express.Router();
 router.route("/signup").post(authController.createUser); // https://localhost:3000/users/signup
 router.route("/login").post(authController.loginUser);
 router.route("/logout").get(authController.logoutUser);
+router.route("/dashboard").get(authController.getDashboardPage); // https://localhost:3000/users/dashboard
 module.exports = router;
